878f3d66a6042bbd64ddb6237fb2cf5c683e6116
Ubuntu <ubuntu@ip-172-31-59-230.us-west-2.compute.internal>
2020-06-24 17:23:15 +0000

    deserialize functions

--- tools/framac/deserializeFunctions.c
+++ tools/framac/deserializeFunctions.c
@@ -2,23 +2,195 @@
 #include <limits.h>
 #include <stdint.h>
 
-// struct MQTTPacketInfo
-// {
-//     /**
-//      * @brief Type of incoming MQTT packet.
-//      */
-//     uint8_t type;
+/*
+struct MQTTPacketInfo
+    {
+        *
+        * @brief Type of incoming MQTT packet.
+        * 
+        uint8_t type;
 
-//     /**
-//      * @brief Remaining serialized data in the MQTT packet.
-//      */
-//     uint8_t * pRemainingData;
+        *
+        * @brief Remaining serialized data in the MQTT packet.
+        *
+        uint8_t * pRemainingData;
 
-//     /**
-//      * @brief Length of remaining serialized data.
-//      */
-//     size_t remainingLength;
-// };
+        *
+        * @brief Length of remaining serialized data.
+        *
+        size_t remainingLength;
+    };
+*/
+
+/*@
+    requires \valid(pIncomingPacket);
+    requires \valid(pPacketId);
+    requires \valid(pSessionPresent);
+    requires 0 <= pIncomingPacket->remainingLength <= SIZE_MAX;
+    requires 0 <= (*pIncomingPacket->pRemainingData << 8)  <= 1 << 16;
+    requires \valid(pIncomingPacket->pRemainingData + (0 .. pIncomingPacket->remainingLength - 1));
+    assigns *pPacketId, *pSessionPresent;
+
+    behavior badInput:
+        assumes pIncomingPacket == ((void *)0) ||
+            ( ( pPacketId == ((void *)0) ) &&
+            ( ( pIncomingPacket->type != ( ( uint8_t ) 0x20U ) ) &&
+            ( pIncomingPacket->type != ( ( uint8_t ) 0xD0U ) ) ) ) ||
+            ( ( pSessionPresent == ((void *)0) ) && ( pIncomingPacket->type == ( ( uint8_t ) 0x20U ) ) ) ||
+            ( pIncomingPacket->pRemainingData == ((void *)0) );
+        ensures *pPacketId == \old(*pPacketId);
+        ensures *pSessionPresent == \old(*pSessionPresent); 
+        ensures \result == MQTTBadParameter;
+
+    disjoint behaviors;
+*/
+MQTTStatus_t MQTT_DeserializeAck( const MQTTPacketInfo_t * const pIncomingPacket,
+                                  uint16_t * const pPacketId,
+                                  bool * const pSessionPresent )
+{
+    MQTTStatus_t status = MQTTSuccess;
+
+    if( ( pIncomingPacket == NULL ) )
+    {
+        LogError( ( "pIncomingPacket cannot be NULL." ) );
+        status = MQTTBadParameter;
+    }
+
+    /* Pointer for packet identifier cannot be NULL for packets other than
+     * CONNACK and PINGRESP. */
+    else if( ( pPacketId == NULL ) &&
+             ( ( pIncomingPacket->type != MQTT_PACKET_TYPE_CONNACK ) &&
+               ( pIncomingPacket->type != MQTT_PACKET_TYPE_PINGRESP ) ) )
+    {
+        LogError( ( "pPacketId cannot be NULL for packet type %02x.",
+                    pIncomingPacket->type ) );
+        status = MQTTBadParameter;
+    }
+    /* Pointer for session present cannot be NULL for CONNACK. */
+    else if( ( pSessionPresent == NULL ) &&
+             ( pIncomingPacket->type == MQTT_PACKET_TYPE_CONNACK ) )
+    {
+        LogError( ( "pSessionPresent cannot be NULL for CONNACK packet." ) );
+        status = MQTTBadParameter;
+    }
+    else if( pIncomingPacket->pRemainingData == NULL )
+    {
+        LogError( ( "Remaining data of incoming packet is NULL." ) );
+        status = MQTTBadParameter;
+    }
+    else
+    {
+        /* Make sure response packet is a valid ack. */
+        switch( pIncomingPacket->type )
+        {
+            case MQTT_PACKET_TYPE_CONNACK:
+                status = deserializeConnack( pIncomingPacket, pSessionPresent );
+                break;
+
+            case MQTT_PACKET_TYPE_SUBACK:
+                status = deserializeSuback( pIncomingPacket, pPacketId );
+                break;
+
+            case MQTT_PACKET_TYPE_PINGRESP:
+                status = deserializePingresp( pIncomingPacket );
+                break;
+
+            case MQTT_PACKET_TYPE_UNSUBACK:
+            case MQTT_PACKET_TYPE_PUBACK:
+            case MQTT_PACKET_TYPE_PUBREC:
+            case MQTT_PACKET_TYPE_PUBREL:
+            case MQTT_PACKET_TYPE_PUBCOMP:
+                status = deserializeSimpleAck( pIncomingPacket, pPacketId );
+                break;
+
+            /* Any other packet type is invalid. */
+            default:
+                LogError( ( "IotMqtt_DeserializeResponse() called with unknown packet type:(%02x).", pIncomingPacket->type ) );
+                status = MQTTBadResponse;
+                break;
+        }
+    }
+
+    return status;
+}
+
+/*@
+    requires \valid(pAck);
+    requires \valid(pPacketIdentifier);
+    requires 0 <= pAck->remainingLength <= SIZE_MAX;
+    requires \valid(pAck->pRemainingData + (0 .. pAck->remainingLength - 1));
+    requires 0 <= (*pAck->pRemainingData << 8)  <= 1 << 16;
+    assigns *pPacketIdentifier;
+    ensures pAck->remainingLength != ( ( uint8_t ) 2 ) ==> *pPacketIdentifier == \old(*pPacketIdentifier) && \result == MQTTBadResponse;
+    ensures pAck->remainingLength == ( ( uint8_t ) 2 ) ==> *pPacketIdentifier == ( uint16_t ) ( ( ( ( uint16_t ) ( *( pAck->pRemainingData ) ) ) << 8 ) | ( ( uint16_t ) ( *( ( pAck->pRemainingData ) + 1 ) ) ) );
+    ensures pAck->remainingLength == ( ( uint8_t ) 2 ) && *pPacketIdentifier == 0U ==> \result == MQTTBadResponse;
+    ensures pAck->remainingLength == ( ( uint8_t ) 2 ) && *pPacketIdentifier != 0U ==> \result == MQTTSuccess;
+
+*/
+static MQTTStatus_t deserializeSimpleAck( const MQTTPacketInfo_t * const pAck,
+                                          uint16_t * pPacketIdentifier )
+{
+    MQTTStatus_t status = MQTTSuccess;
+
+    // assert( pAck != NULL );
+   ((void) sizeof ((pAck != ((void *)0)) ? 1 : 0), __extension__ ({ if (
+    pAck != ((void *)0)) ; else __assert_fail ("pAck != NULL", "mqtt_lightweight.c", 1115, __extension__ __PRETTY_FUNCTION__); }));
+    
+    // assert( pPacketIdentifier != NULL );
+   ((void) sizeof ((pPacketIdentifier != ((void *)0)) ? 1 : 0), __extension__ ({ if (
+    pPacketIdentifier != ((void *)0)) ; else __assert_fail ("pPacketIdentifier != NULL", "mqtt_lightweight.c", 1116, __extension__ __PRETTY_FUNCTION__); }));
+
+    /* Check that the "Remaining length" of the received ACK is 2. */
+    if( pAck->remainingLength != ( ( uint8_t ) 2 ) )
+    {
+        printf( "ACK does not have remaining length of %d.",( ( uint8_t ) 2 ) );
+
+        status = MQTTBadResponse;
+    }
+    else
+    {
+        /* Extract the packet identifier (third and fourth bytes) from ACK. */
+        *pPacketIdentifier = ( uint16_t ) ( ( ( ( uint16_t ) ( *( pAck->pRemainingData ) ) ) << 8 ) | ( ( uint16_t ) ( *( ( pAck->pRemainingData ) + 1 ) ) ) );
+
+        printf( "Packet identifier %hu.", *pPacketIdentifier);
+
+        /* Packet identifier cannot be 0. */
+        if( *pPacketIdentifier == 0U )
+        {
+            status = MQTTBadResponse;
+        }
+    }
+
+    return status;
+}
+
+
+/*@
+    requires \valid(pPingresp);
+    requires 0 <= pPingresp->remainingLength <= SIZE_MAX;
+    requires \valid(pPingresp->pRemainingData + (0 .. pPingresp->remainingLength - 1));
+    assigns \nothing;
+    ensures pPingresp->remainingLength != ( 0U ) ==> \result == MQTTBadResponse;
+    ensures pPingresp->remainingLength == ( 0U ) ==> \result == MQTTSuccess;
+*/
+static MQTTStatus_t deserializePingresp( const MQTTPacketInfo_t * const pPingresp )
+{
+    MQTTStatus_t status = MQTTSuccess;
+
+    // assert( pPingresp != NULL );
+   ((void) sizeof ((pPingresp != ((void *)0)) ? 1 : 0), __extension__ ({ if (
+   pPingresp != ((void *)0)) ; else __assert_fail ("pPingresp != NULL", "mqtt_lightweight.c", 1149, __extension__ __PRETTY_FUNCTION__); }));
+
+    /* Check the "Remaining length" (second byte) of the received PINGRESP is 0. */
+    if( pPingresp->remainingLength != ( 0U ) )
+    {
+        printf( "PINGRESP does not have remaining length of %d.", ( 0U ));
+
+        status = MQTTBadResponse;
+    }
+
+    return status;
+}
 
 /*@
     requires 0 <= statusCount < SIZE_MAX;
@@ -92,8 +264,7 @@ static MQTTStatus_t readSubackStatus( size_t statusCount,
     requires 0 <= pSuback->remainingLength <= SIZE_MAX;
     requires \valid(pSuback->pRemainingData + (0 .. pSuback->remainingLength - 1));
     requires \valid(pPacketIdentifier);
-    requires 0 <= ( *( pSuback->pRemainingData ) ) << 8  <= 1<<16;
-    requires 0 <= *( ( pSuback->pRemainingData ) + 1 ) <= 1<<16;
+    requires 0 <= (*pSuback->pRemainingData << 8)  <= 1<<16;
     requires 0 <= *pPacketIdentifier <= UINT_MAX;
     assigns *pPacketIdentifier;
 
@@ -104,7 +275,10 @@ static MQTTStatus_t readSubackStatus( size_t statusCount,
     
     behavior goodInput:
         assumes pSuback->remainingLength >= 3U;
-
+        ensures *pPacketIdentifier == ( uint16_t ) ( ( ( ( uint16_t ) ( *( pSuback->pRemainingData ) ) ) << 8 ) | ( ( uint16_t ) ( *( ( pSuback->pRemainingData ) + 1 ) ) ) );
+    
+    complete behaviors;
+    disjoint behaviors;
 */
 static MQTTStatus_t deserializeSuback( const MQTTPacketInfo_t * const pSuback,
                                        uint16_t * pPacketIdentifier )
@@ -158,7 +332,7 @@ static MQTTStatus_t deserializeSuback( const MQTTPacketInfo_t * const pSuback,
         ensures \result == MQTTBadResponse;
     
     behavior badInputB:
-        assumes (pConnack->pRemainingData[0] | 0x01U ) != 0x01U;
+        assumes pConnack->remainingLength == ( uint8_t ) 2U && (pConnack->pRemainingData[0] | 0x01U ) != 0x01U;
         ensures *pSessionPresent == \old(*pSessionPresent);
         ensures \result == MQTTBadResponse;
 
@@ -169,6 +343,9 @@ static MQTTStatus_t deserializeSuback( const MQTTPacketInfo_t * const pSuback,
         ensures ((pConnack->pRemainingData[ 0 ] & ( uint8_t ) 0x01U )  == ( uint8_t ) 0x01U) && (pConnack->pRemainingData[ 1 ] == 0U) && (pConnack->pRemainingData[ 1 ] > 5U) ==> \result == MQTTBadResponse;
         ensures ((pConnack->pRemainingData[ 0 ] & ( uint8_t ) 0x01U )  == ( uint8_t ) 0x01U) && (pConnack->pRemainingData[ 1 ] == 0U) && (0U < pConnack->pRemainingData[ 1 ] <= 5U) ==> \result == MQTTServerRefused;
         ensures ((pConnack->pRemainingData[ 0 ] & ( uint8_t ) 0x01U )  == ( uint8_t ) 0x01U) && (pConnack->pRemainingData[ 1 ] == 0U) && (pConnack->pRemainingData[ 1 ] == 0U) ==> \result == MQTTSuccess;
+    
+    complete behaviors;
+    disjoint behaviors;
 */
 static MQTTStatus_t deserializeConnack( const MQTTPacketInfo_t * const pConnack,
                                         bool * const pSessionPresent )
--- tools/framac/deserializeFunctions.h
+++ tools/framac/deserializeFunctions.h
@@ -4,24 +4,16 @@
 #include "mqtt.h"
 #include "mqtt_lightweight.h"
 
-extern int printf(const char *__restrict __format, ...);
+// Main function
+MQTTStatus_t MQTT_DeserializeAck( const MQTTPacketInfo_t * const pIncomingPacket,
+                                  uint16_t * const pPacketId,
+                                  bool * const pSessionPresent );    
 
-/*@
- assigns \nothing;
-*/
-static void logConnackResponse( uint8_t responseCode );
-                  
-/*@
- assigns \nothing;
-*/
-extern void __assert_fail (const char *__assertion, const char *__file,
-      unsigned int __line, const char *__function)
-     __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
+// Helper functions 
+static MQTTStatus_t deserializePingresp( const MQTTPacketInfo_t * const pPingresp );
 
-
-static MQTTStatus_t readSubackStatus( size_t statusCount,
-                                      const uint8_t * pStatusStart);
-
+static MQTTStatus_t deserializeSimpleAck( const MQTTPacketInfo_t * const pAck,
+                                          uint16_t * pPacketIdentifier );
 
 static MQTTStatus_t deserializeSuback( const MQTTPacketInfo_t * const pSuback,
                                        uint16_t * pPacketIdentifier );
@@ -29,20 +21,29 @@ static MQTTStatus_t deserializeSuback( const MQTTPacketInfo_t * const pSuback,
 static MQTTStatus_t deserializeConnack( const MQTTPacketInfo_t * const pConnack,
                                         bool * const pSessionPresent );
 
+static MQTTStatus_t readSubackStatus( size_t statusCount,
+                                      const uint8_t * pStatusStart);       
+
+extern int printf(const char *__restrict __format, ...);
+
+#define LogDebug( message )
+#define LogError( message )
+#define MQTT_PACKET_SIMPLE_ACK_REMAINING_LENGTH     ( ( uint8_t ) 2 ) /**< @brief PUBACK, PUBREC, PUBREl, PUBCOMP, UNSUBACK Remaining length. */
+
+extern void assert(bool assertion);
+
+/*@
+ assigns \nothing;
+*/
+static void logConnackResponse( uint8_t responseCode );
 
 
 
-// /*@
-//  assigns \nothing;
-// */
-// void LogDebug(size_t *__restrict __format, ...);
-
-// static MQTTStatus_t deserializePublish( const MQTTPacketInfo_t * const pIncomingPacket,
-//                                         uint16_t * const pPacketId,
-//                                         MQTTPublishInfo_t * const pPublishInfo );
-
-// static MQTTStatus_t checkPublishRemainingLength( size_t remainingLength,
-//                                                  MQTTQoS_t qos,
-//                                                  size_t qos0Minimum );
+/*@
+ assigns \nothing;
+*/
+extern void __assert_fail (const char *__assertion, const char *__file,
+      unsigned int __line, const char *__function)
+     __attribute__ ((__nothrow__ , __leaf__)) __attribute__ ((__noreturn__));
 
 #endif                                    
